b2ceb291bbd2ae379d8e2308bbe93a57cf41dd82
Mayderking <163465498+Mayderking@users.noreply.github.com>
2024-09-28 21:22:55 -0500

    vistas
    se agregaron las funciones necesarias en el archivo views.py
    para productos, clientes, ventas, proveedores y categorias

---
+++ gestion_ventas/inventario/views.py
@@ -0,0 +1,187 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Producto, Proveedor, Cliente, Categoria, Venta
+from .forms import ProductoForm, ClienteForm, VentaForm, CategoriaForm, ProveedorForm
+from django.db.models import Q
+from django.core.paginator import Paginator
+
+# Create your views here.
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+
+def listar_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'listar_productos.html', {'productos': productos})
+
+def detalle_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, 'detalle_producto.html', {'producto': producto})
+
+def agregar_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'agregar_producto.html', {'form': form})
+
+def editar_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm(instance=producto)
+    return render(request, 'editar_producto.html', {'form':form})
+
+def eliminar_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    if request.method == 'POST':
+        producto.delete()
+        return redirect('listar_productos')
+    return render(request, 'eliminar_producto.html', {'producto': producto})
+
+
+def listar_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'listar_clientes.html', {'clientes':clientes})
+
+def agregar_cliente(request):
+    if request.method == 'POST':
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_clientes')
+    else:
+        form = ClienteForm()
+    return render(request, 'agregar_cliente.html', {'form':form})
+
+def editar_cliente(request, pk):    
+    cliente = get_object_or_404(Cliente, pk=pk)
+    if request.method == 'POST':
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_clientes')
+    else:
+        form = ClienteForm(instance=cliente)
+        return render(request, 'editar_cliente.html', {'form': form})
+
+def eliminar_cliente(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    if request.method == 'POST':
+        cliente.delete()
+        return redirect('listar_clientes')
+    return render(request, 'eliminar_cliente.html', {'cliente':cliente})
+
+
+def listar_ventas(request):
+    ventas = Venta.objects.all().select_related('producto', 'cliente')
+    return render(request, 'listar_ventas.html', {'ventas': ventas})
+
+def agregar_venta(request):
+    if request.method == 'POST':
+        form = VentaForm(request.POST)
+        if form.is_valid():
+            venta = form.save(commit=False)
+            producto = venta.producto
+            if producto.stock >= venta.cantidad:
+                venta.total = producto.precio * venta.cantidad
+                producto.stock -= venta.cantidad
+                producto.save()
+                venta.save()
+                return redirect('listar_ventas')
+            else:
+                form.add_error('cantidad', 'No hay el stock suficiente realizar esta venta.')
+    else:
+        form = VentaForm()
+    return render(request, 'agregar_venta.html', {'form':form})
+
+def reporte_ventas(request):
+    ventas = Venta.objects.all()
+    fecha_inicio = request.GET.get('fecha_inicio')
+    fecha_fin = request.GET.get('fecha_fin')
+    cliente_id = request.GET.get('cliente_id')
+    producto_id = request.GET.get('producto_id')
+
+    if fecha_inicio and fecha_fin:
+        ventas = ventas.filter(fecha_venta__range=[parse_date(fecha_inicio), parse_date(fecha_fin)])
+
+    if cliente_id:
+        ventas = ventas.filter(cliente_id=cliente_id)
+
+    if producto_id:
+        ventas = ventas.filter(producto_id=producto_id)
+
+    return render(request, 'reporte_ventas.html', {'ventas': ventas})
+
+
+def listar_categorias(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'listar_categorias.html', {'categorias': categorias})
+
+def agregar_categoria(request):
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_categorias')
+    else:
+        form = CategoriaForm()
+    return render(request, 'agregar_categoria.html', {'form': form})
+
+def editar_categoria(request, pk):
+    categoria = get_object_or_404(Categoria, pk=pk)
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_categorias')
+    else:
+        form = CategoriaForm(instance=categoria)
+    return render(request, 'editar_categoria.html', {'form': form})
+
+def eliminar_categoria(request, pk):
+    categoria = get_object_or_404(Categoria, pk=pk)
+    if request.method == 'POST':
+        categoria.delete()
+        return redirect('listar_categorias')
+    return render(request, 'eliminar_categoria.html', {'categoria': categoria})
+
+
+def listar_proveedores(request):
+    proveedores = Proveedor.objects.all()
+    return render(request, 'listar_proveedores.html', {'proveedores': proveedores})
+
+def agregar_proveedor(request):
+    if request.method == 'POST':
+        form = ProveedorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_proveedores')
+    else:
+        form = ProveedorForm()
+    return render(request, 'agregar_proveedor.html', {'form': form})
+
+def editar_proveedor(request, pk):
+    proveedor = get_object_or_404(Proveedor, pk=pk)
+    if request.method == 'POST':
+        form = ProveedorForm(request.POST, instance=proveedor)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_proveedores')
+    else:
+        form = ProveedorForm(instance=proveedor)
+    return render(request, 'editar_proveedor.html', {'form': form})
+
+def eliminar_proveedor(request, pk):
+    proveedor = get_object_or_404(Proveedor, pk=pk)
+    if request.method == 'POST':
+        proveedor.delete()
+        return redirect('listar_proveedores')
+    return render(request, 'eliminar_proveedor.html', {'proveedor': proveedor})
